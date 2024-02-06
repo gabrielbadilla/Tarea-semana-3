87cfc9a6b8f5287d3be9443086080d8e39aa6682
gabrielbadilla <gbadilla60348@ufide.ac.cr>
2024-02-05 23:56:54 -0600

    primera tienda

---
+++ TareaSemana3/src/main/java/TareaSemana3/demo/TareaSemana3Application.java
@@ -0,0 +1,13 @@
+package TareaSemana3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaSemana3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaSemana3Application.class, args);
+	}
+
+}
---
+++ TareaSemana3/src/test/java/TareaSemana3/demo/TareaSemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package TareaSemana3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSemana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
